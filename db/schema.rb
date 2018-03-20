e95900b30fbaa4e08ade98b73c780eb0ef923ed6
Brennan Heisler <learn@heislercreative.com>
2018-03-20 00:31:19 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319233535) do
+ActiveRecord::Schema.define(version: 20180320002939) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.float "balance"
+    t.decimal "balance"
   end
 
 end
